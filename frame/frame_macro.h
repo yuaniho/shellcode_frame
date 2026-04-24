c3fc9a47d733e72cf6c2292d4b9694549bb14f69
Yuan <3051596481@qq.com>
2026-04-24 13:57:27 +0800

    优化代码内容，实现真正意义上的 SHELLCODE_ENTRY 宏

---
+++ frame/frame_macro.h
@@ -0,0 +1,16 @@
+//
+// Created by YUAN on 2026/4/23.
+//
+
+// 定义统一的调用头文件，方便使用框架内部的相关宏以及函数
+
+#ifndef SHELLCODE_FRAME_MACRO_H
+#define SHELLCODE_FRAME_MACRO_H
+
+#include "frame_basic_macro.h"
+#include "function_signature.h"
+#include "frame_constant_macro.h"
+#include "../api/api.h"
+#include "../frame_api/frame_api.h"
+
+#endif //SHELLCODE_FRAME_MACRO_H
